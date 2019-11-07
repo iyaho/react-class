faf04edfe5bb535f6d6e2d41c65ff94942da6dff
iyaho <dlwlsgh0808@gmail.com>
2019-11-07 17:29:28 +0900

    google

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./img/googlelogo.png";
+import apps from "./img/apps.png";
+import user from "./img/circle.png";
 import './App.css';
 
 function App() {
   return (
     <div className="root">
-      <header>Gmail 이미지 </header>
-      <nav><img src="../googlelogo.png" alt="경로 설정하셈"></img></nav>
+      <header><a href="https://mail.google.com/">Gmail</a><a href="https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl">이미지</a><img src={apps} alt="앱" className="apps"/><img src={user} alt="user" className="user"/></header>
+      <div className="logo"><img src={logo} alt="구글 로고"/></div>
+      <nav></nav>
       <div>
         <section>main</section>
         <aside>aside</aside>
