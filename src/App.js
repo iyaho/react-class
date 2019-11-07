b84e9d049351c8f4c71752e8bd7aadf4798f9a48
iyaho <dlwlsgh0808@gmail.com>
2019-11-07 19:34:26 +0900

    google 날아갔음 ㅜㅜ

--- src/App.js
+++ src/App.js
@@ -6,16 +6,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="root">
-      <header><a href="https://mail.google.com/">Gmail</a><a href="https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl">이미지</a><img src={apps} alt="앱" className="apps"/><img src={user} alt="user" className="user"/></header>
-      <div className="logo"><img src={logo} alt="구글 로고"/></div>
-      <nav></nav>
-      <div>
-        <section>main</section>
-        <aside>aside</aside>
+      <>
+      <div style={{border: "1px solid black",backgroundColor:"f00"}}>
+          hello world
       </div>
-      <footer>footer</footer>
-    </div>
+      </>
   );
 }
 export default App;
