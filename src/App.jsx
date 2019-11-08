04e7f3f95420ae54d7be0298eedb93b0aed99812
unknown <dlwlsgh0808@gmail.com>
2019-11-08 16:37:48 +0900

    dfa

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Student from './Student';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <Student/>
+    );
+  }
+}
+
+export default App;
