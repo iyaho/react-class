7081f3c451c6900ee9cb6b1abf83ff8d75319d8e
unknown <dlwlsgh0808@gmail.com>
2019-11-12 21:01:59 +0900

    2019.11.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import TodoItem from "./Todo";
+import Todo from "./Todo";
 import './app.css'
 
 class App extends React.Component {
@@ -24,13 +24,32 @@ class App extends React.Component {
     this.setState({ value: e.target.value });
   };
 
+  onClickDel= idx =>{
+    const items = this.state.items;
+    this.setState({
+      items:[...items.slice(0,idx),...items.slice(idx+1)]
+    });
+    console.log(idx);
+  };
+
+  onClickMod=(value,idx) => {
+    const newItems=[
+      ...this.state.items.slice(0,idx),
+      value,
+      ...this.state.items.slice(idx+1)
+    ];
+    this.setState({
+      items:newItems
+    });
+  };
+  
   render() {
     return (
       <div>
-        <input type="text" onChange={this.onChange} />
-        <button onClick={this.onClickAdd}>추가</button>
+        <input type="text" onChange={this.onChange}/>
+        <button onClick={this.onClickAdd} >추가</button>
         {this.state.items.map((value, idx) => (
-          <TodoItem key={idx} value={value} />
+          <Todo key={Math.random()} value={value} onClickDel={()=>{this.onClickDel(idx)}} onClickMod={()=>{var value=prompt();this.onClickMod(value,idx)}}/>
         ))}
       </div>
     );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,13 +5,18 @@ import './app.css'
 class TodoItem extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state= ({
+      value:props.value,
+      items:props.items
+    })
   }
   render() {
     return (
       <div className="div">
         <span>{this.props.value}</span>
-        <button className="button" >수정</button>
-        <button className="button">삭제</button>
+        <button className="button"onClick={this.props.onClickMod} >수정</button>
+        <button className="button" onClick={this.props.onClickDel}>삭제</button>
       </div>
     );
   }
